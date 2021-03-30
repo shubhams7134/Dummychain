a53297ad5ee6dccddb89c819c78cfef636747499
Shubham Kumar Singh <shubhams7134@gmail.com>
2021-03-30 15:43:40 +0530

    decentralized and functionalities added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+const sha256 = require('sha256');
+const { v4: uuidv4 } = require('uuid');
 function Blockchain () {
     this.chain=[];
     this.pendingTransactions=[];
+    this.networkNodes = [];
+    this.createNewBlock(51,'8','8');
 }
 
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash){
@@ -18,6 +22,69 @@ Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash){
 
     return newBlock;
     } 
+
+Blockchain.prototype.getLastBlock = function (){
+    return this.chain[this.chain.length-1];
+}
+
+Blockchain.prototype.createNewTransaction = function (amount,sender,recipient){
+    const newTransaction={
+        amount: amount,
+        sender: sender,
+        recipient: recipient,
+        transactionId: uuidv4().split('-').join('')
+    };
+
+    return newTransaction;
+    // this.pendingTransactions.push(newTransaction);
+
+    // return this.getLastBlock()['index'] + 1;
+}
+Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj){
+
+        this.pendingTransactions.push(transactionObj);
+        return this.getLastBlock()['index'] + 1;
+
+}
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+	const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+	const hash = sha256(dataAsString);
+	return hash;
+};
+Blockchain.prototype.chainIsValid = function(blockchain) {
+	let validChain = true;
+
+	for (var i = 1; i < blockchain.length; i++) {
+		const currentBlock = blockchain[i];
+		const prevBlock = blockchain[i - 1];
+		const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+		if (blockHash.substring(0, 4) !== '0000') validChain = false;
+		if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
+	};
+
+	const genesisBlock = blockchain[0];
+	const correctNonce = genesisBlock['nonce'] === 100;
+	const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+	const correctHash = genesisBlock['hash'] === '0';
+	const correctTransactions = genesisBlock['transactions'].length === 0;
+
+	if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+	return validChain;
+};
+
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+	let nonce = 0;
+	let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+	while (hash.substring(0, 4) !== '0000') {
+		nonce++;
+		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        //console.log(hash);
+	}
+
+	return nonce;
+};
 
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,30 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
-const bitcoin2 = new Blockchain();
+// const bitcoin2 = new Blockchain();
 
-bitcoin.createNewBlock(555,"@00000","@222");
-bitcoin.createNewBlock(556,"@00100","@232");
-bitcoin.createNewBlock(575,"@01000","@223");
+// bitcoin.createNewBlock(555,"@00000","@222");
+// bitcoin.createNewBlock(556,"@00100","@232");
+// bitcoin.createNewBlock(575,"@01000","@223");
+// bitcoin.createNewTransaction(1000,"Shubham","Vivek");
+// bitcoin.createNewBlock(575,"@01000","@223");
+const previousBlockHash='58511abe2d7fa1a6fd51a140b474baef65a8aa91a800d01a7b7541716d1eebe1';
+const currentBlockData= [
+    {
+        amount: 101,
+        sender:'Shubham',
+        recipient:'Ram'
+    },
+    {
+        amount: 102,
+        sender:'Shubham',
+        recipient:'Shyam'
+    },
+    {
+        amount: 102,
+        sender:'Shubham',
+        recipient:'Abdul'
+    }
+];
+console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
+console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,32644));
 console.log(bitcoin);
-console.log(bitcoin2);
